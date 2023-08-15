84c4113cc85c93deb5fdf4ccb85ae648e5eaa38d
ManEnfu <manenfu@protonmail.com>
2023-08-16 00:00:41 +0700

    core: add `Pattern`

--- khazanah-core/src/phonology.rs
+++ khazanah-core/src/phonology.rs
@@ -2,10 +2,12 @@ mod categories;
 mod category;
 mod error;
 mod inventory;
+mod pattern;
 mod phoneme;
 
 pub use categories::Categories;
 pub use category::Category;
 pub use error::Error;
 pub use inventory::Inventory;
+pub use pattern::{Pattern, PatternElement, PatternElements};
 pub use phoneme::{Phoneme, PhonemeBuilder};
---
+++ khazanah-core/src/phonology/pattern.rs
@@ -0,0 +1,319 @@
+use std::str::Chars;
+
+use super::{Categories, Category, Error};
+
+use crate::xml::{ReadXml, WriteXml, XmlError, XmlReader, XmlWriter};
+
+/// An element of a pattern.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PatternElement<'a> {
+    Str(&'a str),
+    Category(&'a Category),
+}
+
+#[derive(Debug, Clone)]
+pub struct PatternElements<'a> {
+    pattern: &'a str,
+    p_iter: Chars<'a>,
+    categories: &'a Categories,
+    index: usize,
+    next_char: Option<char>,
+}
+
+impl<'a> PatternElements<'a> {
+    pub fn new(pattern: &'a Pattern, categories: &'a Categories) -> Self {
+        Self {
+            pattern: pattern.pattern_str.as_str(),
+            p_iter: pattern.pattern_str.chars(),
+            categories,
+            index: 0,
+            next_char: None,
+        }
+    }
+}
+
+impl<'a> Iterator for PatternElements<'a> {
+    type Item = PatternElement<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let p_len = self.pattern.len();
+        if self.index >= p_len {
+            None
+        } else {
+            let init_char = self.next_char.or_else(|| self.p_iter.next())?;
+            let mut end_index = self.index + init_char.len_utf8();
+
+            let is_category = init_char.is_ascii_uppercase();
+
+            loop {
+                self.next_char = self.p_iter.next();
+
+                if let Some(nc) = self.next_char {
+                    if (is_category && nc.is_ascii_digit())
+                        || (!is_category && !nc.is_ascii_uppercase())
+                    {
+                        end_index += nc.len_utf8();
+                        continue;
+                    }
+                }
+                break;
+            }
+
+            let slice = self.pattern.get(self.index..end_index)?;
+            self.index = end_index;
+
+            let elem = if is_category {
+                if let Some(cat) = self.categories.category_by_name(slice) {
+                    PatternElement::Category(cat)
+                } else {
+                    PatternElement::Str(slice)
+                }
+            } else {
+                PatternElement::Str(slice)
+            };
+
+            Some(elem)
+        }
+    }
+}
+
+/// A syllable pattern. The pattern string can contains IPA symbols and category names.
+///
+/// For example: `"CrV"` means that the pattern consists of a consonant, followed by /r/,
+/// followed by a vowel, forming something like /kra/ or /tri/.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Pattern {
+    /// The pattern string.
+    pub pattern_str: String,
+    /// The mora or weight of the pattern.
+    pub mora: u32,
+    /// The likeliness of this pattern to occur.
+    pub likeliness: usize,
+}
+
+impl Pattern {
+    /// Creates a new pattern.
+    pub fn new(s: String) -> Self {
+        Self {
+            pattern_str: s,
+            mora: 1,
+            likeliness: 1,
+        }
+    }
+
+    /// Parse and iterates pattern string into elements.
+    pub fn parse_elements<'a>(
+        &'a self,
+        categories: &'a Categories,
+    ) -> impl Iterator<Item = PatternElement> {
+        PatternElements::new(self, categories)
+    }
+}
+
+impl Default for Pattern {
+    fn default() -> Self {
+        Self {
+            pattern_str: Default::default(),
+            mora: 1,
+            likeliness: 1,
+        }
+    }
+}
+
+impl ReadXml for Pattern {
+    type Error = Error;
+
+    type ReaderState = ();
+
+    const TAG: &'static str = "pattern";
+
+    fn process_tag_start<R: std::io::BufRead>(
+        &mut self,
+        reader: &mut XmlReader<R>,
+        _state: &mut Self::ReaderState,
+        name: String,
+        _attrs: Vec<(String, String)>,
+    ) -> Result<(), XmlError<Self::Error>> {
+        match reader.last_tag_pair() {
+            (_, Some(Self::TAG)) => {}
+            (Some(Self::TAG), Some("pattern-str")) => {
+                self.pattern_str.clear();
+            }
+            (Some(Self::TAG), Some("mora")) => {
+                self.mora = 1;
+            }
+            (Some(Self::TAG), Some("likeliness")) => {
+                self.likeliness = 1;
+            }
+            _ => return Err(XmlError::InvalidTag(name)),
+        }
+        Ok(())
+    }
+
+    fn process_text<R: std::io::BufRead>(
+        &mut self,
+        reader: &mut XmlReader<R>,
+        _state: &mut Self::ReaderState,
+        text: String,
+    ) -> Result<(), XmlError<Self::Error>> {
+        match reader.last_tag() {
+            Some("pattern-str") => {
+                self.pattern_str += &text;
+            }
+            Some("mora") => {
+                self.mora = text.parse().unwrap_or(1);
+            }
+            Some("likeliness") => {
+                self.likeliness = text.parse().unwrap_or(1);
+            }
+            _ => {}
+        }
+
+        Ok(())
+    }
+
+    fn process_tag_end<R: std::io::BufRead>(
+        &mut self,
+        _reader: &mut XmlReader<R>,
+        _state: &mut Self::ReaderState,
+        _name: String,
+    ) -> Result<(), XmlError<Self::Error>> {
+        Ok(())
+    }
+}
+
+impl WriteXml for Pattern {
+    type Error = Error;
+
+    fn serialize_xml<W: std::io::Write>(
+        &self,
+        writer: &mut XmlWriter<W>,
+    ) -> Result<(), XmlError<Self::Error>> {
+        writer.write_tag_start("pattern")?;
+
+        writer.write_tag_start("pattern-str")?;
+        writer.write_text(self.pattern_str.as_str())?;
+        writer.write_tag_end("pattern-str")?;
+
+        writer.write_tag_start("mora")?;
+        writer.write_text(self.mora.to_string().as_str())?;
+        writer.write_tag_end("mora")?;
+
+        writer.write_tag_start("likeliness")?;
+        writer.write_text(self.likeliness.to_string().as_str())?;
+        writer.write_tag_end("likeliness")?;
+
+        writer.write_tag_end("pattern")?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const XML1: &str = r#"
+    <pattern>
+        <pattern-str>CVN</pattern-str>
+        <mora>2</mora>
+        <likeliness>3</likeliness>
+    </pattern>
+    "#;
+
+    #[test]
+    fn read_xml() {
+        let pat = Pattern::load_xml_str(XML1).unwrap();
+
+        assert_eq!(&pat.pattern_str, "CVN");
+        assert_eq!(pat.mora, 2);
+        assert_eq!(pat.likeliness, 3);
+    }
+
+    #[test]
+    fn write_xml() {
+        let pat = Pattern::load_xml_str(XML1).unwrap();
+        let xml2 = pat.save_xml_string().unwrap();
+        let pat2 = Pattern::load_xml_str(&xml2).unwrap();
+        assert_eq!(&pat, &pat2);
+    }
+
+    #[test]
+    fn parse_patterns() {
+        let mut cats = Categories::new();
+
+        assert_eq!(
+            Pattern::new("C".to_string())
+                .parse_elements(&cats)
+                .collect::<Vec<_>>(),
+            vec![PatternElement::Str("C"),]
+        );
+        assert_eq!(
+            Pattern::new("CV".to_string())
+                .parse_elements(&cats)
+                .collect::<Vec<_>>(),
+            vec![PatternElement::Str("C"), PatternElement::Str("V"),]
+        );
+        assert_eq!(
+            Pattern::new("CVC".to_string())
+                .parse_elements(&cats)
+                .collect::<Vec<_>>(),
+            vec![
+                PatternElement::Str("C"),
+                PatternElement::Str("V"),
+                PatternElement::Str("C"),
+            ]
+        );
+        assert_eq!(
+            Pattern::new("CrV".to_string())
+                .parse_elements(&cats)
+                .collect::<Vec<_>>(),
+            vec![
+                PatternElement::Str("C"),
+                PatternElement::Str("r"),
+                PatternElement::Str("V"),
+            ]
+        );
+        assert_eq!(
+            Pattern::new("C1C2rVN".to_string())
+                .parse_elements(&cats)
+                .collect::<Vec<_>>(),
+            vec![
+                PatternElement::Str("C1"),
+                PatternElement::Str("C2"),
+                PatternElement::Str("r"),
+                PatternElement::Str("V"),
+                PatternElement::Str("N"),
+            ]
+        );
+        assert_eq!(
+            Pattern::new("Cro".to_string())
+                .parse_elements(&cats)
+                .collect::<Vec<_>>(),
+            vec![PatternElement::Str("C"), PatternElement::Str("ro"),]
+        );
+
+        let mut cat = Category::new();
+        cat.set_name("C".to_string());
+        let _ = cats.add_category(cat);
+
+        let mut cat = Category::new();
+        cat.set_name("V".to_string());
+        let _ = cats.add_category(cat);
+
+        let c_ref = cats.category_by_name("C").unwrap();
+        let v_ref = cats.category_by_name("V").unwrap();
+
+        assert_eq!(
+            Pattern::new("CrVC".to_string())
+                .parse_elements(&cats)
+                .collect::<Vec<_>>(),
+            vec![
+                PatternElement::Category(c_ref),
+                PatternElement::Str("r"),
+                PatternElement::Category(v_ref),
+                PatternElement::Category(c_ref),
+            ]
+        );
+    }
+}
